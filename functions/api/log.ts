95b41e5eabe7d776550711663620ebc7a7c09ae2
--replace-all <dols3m@gmail.com>
2023-12-30 02:00:42 -0500

    feat: add GET to log endpoint

--- functions/api/log.ts
+++ functions/api/log.ts
@@ -22,3 +22,28 @@ export const onRequestPost: PagesFunction<Env> = async ({ request, env: { LOGGER
 
 	return res.json({ status: 'saved', ts });
 };
+
+export const onRequestGet: PagesFunction<Env> = async ({ request, env: { LOGGER_KV } }) => {
+	const requestUrl = new URL(request.url);
+	const host = requestUrl.searchParams.get('host');
+	const path = requestUrl.searchParams.get('path');
+
+	let prefix = host ? host : '';
+	if (host && path) prefix += `/${path}`;
+
+	let cursor: string | undefined;
+	const keys = [] as string[];
+	while (true) {
+		const [err1, result] = await flatry(LOGGER_KV.list({ prefix, cursor }));
+		if (err1) return res.json({ error: 'KV request failed', errorDetail: (err1 as Error).stack }, { status: 500 });
+		
+		result.keys.forEach(({ name }) => keys.push(name));
+
+		if (result.list_complete) break;
+		cursor = result.cursor;
+	}
+
+	
+
+	return res.json({ keys });
+};
