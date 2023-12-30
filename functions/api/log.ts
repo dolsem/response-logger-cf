07485d4b9bb1d2486e1d98227112c96d4099f19e
--replace-all <dols3m@gmail.com>
2023-12-30 00:32:57 -0500

    initial

---
+++ functions/api/log.ts
@@ -0,0 +1,24 @@
+import * as res from '../utils/response';
+import flatry from 'flatry';
+
+interface RequestBody {
+	host: string;
+	path: string;
+	content: string;
+}
+
+interface Env {
+  REDDIT_KV: KVNamespace;
+}
+
+export const onRequestPost: PagesFunction<Env> = async ({ request, env: { REDDIT_KV } }) => {
+	const [err1, { host, path, content }] = await flatry(request.json<RequestBody>());
+	if (err1) return res.json({ error: 'Invalid request', errorDetail: (err1 as Error).stack }, { status: 400 });
+
+	const prefix = `${host}/${path}`;
+	const ts = Date.now();
+	const key = `${prefix}#${ts}`;
+	const [err2] = await flatry(REDDIT_KV.put(key, content));
+
+	return res.json({ status: 'saved', ts });
+};
